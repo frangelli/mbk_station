d6a986f3f5861b2dbfe5772d4033bdeef34ff397
José Coelho <contato@josecoelho.com>
2014-08-14 16:55:44 -0300

    fix intima

--- app/models/percept/intimacao.rb
+++ app/models/percept/intimacao.rb
@@ -8,8 +8,8 @@ class Percept::Intimacao < Percept::Base
 
         # precisamos da contagem da situação das intimações no sistema
         # Não Classificadas -> revisado=0
-        # Classificadas -> revisado=1, statusCPJ=0, status_1 = NOSSO
-        # Lançadas (inclui sem prazos)  -> status_1 = NOVO, statusCPJ=1 | status_1 = NOSSO, revisado=2, statusCPJ=0
+        # Classificadas -> revisado=1, statusCPJ=0, status_1 = NOVO
+        # Lançadas (inclui sem prazos)  -> status_1 = NOVO, statusCPJ=1 | status_1 = NOVO, revisado=2, statusCPJ=0
         # Lixeira  -> status_1 = LIXEIRA, revisado = 1
         counts = select("count(*) as count, status_cpj, revisado, status_1")
             .where("data_publicacao BETWEEN ? AND ?",start_date, end_date)
@@ -20,9 +20,9 @@ class Percept::Intimacao < Percept::Base
         lancadas = 0
         lixeira = 0
         counts.each do |count|
-            if count.revisado == 1 and count.status_cpj == 0 and count.status_1 == "NOSSO"
+            if count.revisado == 1 and count.status_cpj == 0 and count.status_1 == "NOVO"
                 classificadas = classificadas + count.count
-            elsif count.status_cpj == 1 || ( count.status_1 == "NOSSO" and count.revisado == 2 )
+            elsif count.status_cpj == 1 || ( count.status_1 == "NOVO" and count.revisado == 2 )
                 lancadas = lancadas + count.count
             elsif count.revisado == 1 and count.status_1 == "LIXEIRA"
                 lixeira = lixeira + count.count
